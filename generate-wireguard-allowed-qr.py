912f7d63450ed8f0c454bce2de65075ccf5dbdd1
Adam Greene <copyright@mzpqnxow.com>
2020-06-20 07:44:25 -0400

    Cleanups, change options

--- generate-wireguard-allowed-qr.py
+++ generate-wireguard-allowed-qr.py
@@ -1,59 +1,41 @@
 #!/usr/bin/env python3
-"""Generated Allowed IPs for WireGuard, see README.md for more details"""
-from argparse import (
-    Action,
-    ArgumentParser)
-from copy import copy
-from ipaddress import (
-    collapse_addresses,
-    ip_network,
-    IPv4Address,
-    IPv4Network)
+"""Generated Allowed IPs for WireGuard by specifying an IPv4 exclusion list"""
+from argparse import (Action, ArgumentParser)
+from ipaddress import (collapse_addresses, ip_network, IPv4Address, IPv4Network)
 from shutil import which
-from subprocess import (
-    CalledProcessError,
-    PIPE,
-    run)
-from sys import (
-    argv,
-    stderr,
-    stdout)
+from subprocess import (CalledProcessError, PIPE, run)
+from sys import (argv, exit, stderr, stdout)
 
-SUPPORTED_IP_VERSION = 4
-IPV4_ANY_NET = '0.0.0.0/0'
+DEFAULT_IP_VERSION = 4
+IPV4_ALL_NET = '0.0.0.0/0'
+IPV6_ALL_NET = '::0/0'
 QR_ENCODE_COMMAND = ['qrencode', '-t', 'ansi']
-G_DEBUG = False
 
 
-def error(msg, fatal=False, exit_code=1):
-    stderr.write('{}: {}\n'.format('FATAL' if fatal is True else 'ERROR', msg))
-    if fatal is True:
+def error(msg, is_fatal=False, exit_code=1):
+    stderr.write('{}: {}\n'.format('FATAL' if is_fatal is True else 'ERROR', msg))
+    if is_fatal is True:
         exit(exit_code)
 
 
 def fatal(msg, exit_code=1):
-    error(msg, fatal=True, exit_code=exit_code)
-
-
-def debug(msg):
-    global G_DEBUG
-    if G_DEBUG is False:
-        return
-    stderr.write('DEBUG: {}\n'.format(msg))
+    error(msg, is_fatal=True, exit_code=exit_code)
 
 
 def output(msg):
     stdout.write('{}\n'.format(msg))
 
 
-def ip_network_safe(value, strict=True):
-    """Make an IPv4Network, handling exceptions and special cases
+def ip_network_safe(value, strict=True, supported_ip_version=None, do_supernet=True):
+    """Make an IPv4Network or IPv4Network list from address or address list
 
     Input
     =====
-    value (None, str, IPv4Network, IPv4Address): A dotted quad address, IPv4 CIDR notation
-                                                 address, or None. If None, return 0/0
+    value (None, str, IPv4Network, IPv4Address, list)
+        A single or list of dotted quad address(es), IPv4 CIDR notation address(es) or None
+        If None, return IPv4Network('0.0.0.0/0')
     strict (bool): CIDR notation must not have host bits set when creating IPv4Network
+    do_supernet (bool): Perform supernet operation if value is list
 
     Return
     ======
@@ -63,13 +45,20 @@ def ip_network_safe(value, strict=True):
     =====
     Exceptions are fatal and can not be caught
 
-
     """
     if value is None:
-        return ip_network(IPV4_ANY_NET)
+        raise ValueError('value must be address (str dotted quad/CIDR, IPv4Network) or list of addresses')
 
-    if not isinstance(value, (IPv4Address, IPv4Network, str)):
+    if supported_ip_version is None:
+        supported_ip_version = DEFAULT_IP_VERSION
+
+    if not isinstance(value, (IPv4Address, IPv4Network, str, list)):
         fatal('{} invalid type for IPv4Network (type:{})'.format(value, type(value)))
+
+    if isinstance(value, (list)):
+        if do_supernet is False:
+            return [ip_network_safe(val, strict=strict) for val in value]
+        return supernet([ip_network_safe(val, strict=strict) for val in value])
 
     try:
         return ip_network(value, strict=strict)
@@ -81,96 +70,50 @@ def ip_network_safe(value, strict=True):
             value, err))
 
 
-def ip_network_list_safe(values, supernet=True, strict=True):
-    """Return a list of IPv4Network objects
-
-    See ip_network_safe for documentation, this is just a wrapper
-
-    Set supernet=True to supernet the list
-
-    """
-    if supernet is False:
-        return [ip_network_safe(value, strict=strict) for value in values]
-    return supernet_list([ip_network_safe(value, strict=strict) for value in values])
-
-
-def exclude_net_from_net(exclude_net, base_network, strict=True):
-    assert isinstance(exclude_net, (str, IPv4Network))
-    assert isinstance(base_network, (str, IPv4Network))
-
-    exclude_net = ip_network_safe(exclude_net, strict=strict)
-    base_network = ip_network_safe(base_network, strict=strict)
-
-    try:
-        network_list = list(base_network.address_exclude(exclude_net))
-        return list(network_list)
-    except ValueError as err:
-        print(err)
-    return [base_network]
-
-
-def exclude_net_from_net_list(exclude_net, base_network_list, strict=True):
-    """Useful for exception handling"""
-    exclude_net = ip_network_safe(exclude_net, strict=strict)
-    base_network_list = ip_network_list_safe(base_network_list, strict=strict)
-
-    looping_base_list = copy(base_network_list)
-    running_include_list = list()
-    for base_net in looping_base_list:
-        try:
-            running_include_list.extend(list(base_net.address_exclude(exclude_net)))
-        except ValueError:
-            # Because of the type checking, ValueError should be harmless
-            debug('{} not a part of {}, skipping'.format(exclude_net, base_net))
-            running_include_list.append(base_net)
-        except Exception as err:
-            fatal('unexpected error ({})'.format(err))
-
-    return running_include_list
-
-
-def exclude_net_list(net_exclusion_list, base_net=None, strict=True):
+def exclude_nets_to_include_nets(net_exclusion_list, base_net=None, strict=True):
     """Exclude a list of networks from a larger network"""
     if isinstance(net_exclusion_list, (list, set)) and not net_exclusion_list:
         return list(base_net)
 
-    net_exclusion_list = ip_network_list_safe(net_exclusion_list, strict=strict)
+    if base_net is None:
+        base_net = IPV4_ALL_NET
+
+    net_exclusion_list = ip_network_safe(net_exclusion_list, strict=strict)
 
     base_net = ip_network_safe(base_net, strict=strict)
+    allowed_ip_list = [base_net]
 
-    first_exclusion = ip_network_safe(net_exclusion_list.pop(), strict=strict)
-    running_list = exclude_net_from_net(first_exclusion, base_net, strict=strict)
+    for net_exclude in net_exclusion_list:
+        running_allowed_netlist = []
+        for working_allowed_ip in allowed_ip_list:
+            if working_allowed_ip.overlaps(net_exclude):
+                running_allowed_netlist.extend(working_allowed_ip.address_exclude(net_exclude))
+                continue
+            running_allowed_netlist.append(working_allowed_ip)
+        allowed_ip_list = running_allowed_netlist
+    return supernet(allowed_ip_list)
 
-    for exclude_net in net_exclusion_list:
-        running_list = exclude_net_from_net_list(
-            ip_network_safe(exclude_net, strict=strict),
-            running_list)
-        running_list = list(running_list)
 
-    return running_list
+def supernet(network_list, strict=False):
+    """Supernet list of addresses/networks
 
-
-def supernet_list(network_list, strict=False):
-    """Supernet a list of ASCII or ipaddress.ip_network objects
-
-    Because ip_network works fine on both ASCII and ip_network objects,
-    network_list can be a mixed set of both str and IPV4Networks. It is
-    redundant to use ip_network on an IPv4Nework, but it simplifies the logic
+    network_list (list): List of addresses as dotted-quad str, CIDR str, IPv4Address
+                         or IPv4Network
 
     Strict is set to False by default, this turns off the logic internal to
     ip_network that will cause an Exception to be raised if the host bits of
     the network are set
     """
-    assert isinstance(network_list, (list, set))
-    return list(collapse_addresses(
+    assert isinstance(network_list, list)
+    supernetwork_list = list(collapse_addresses(
         [ip_network(net, strict=strict) for net in network_list]))
+    return supernetwork_list
 
 
 class CSVNetworkList(Action):
-    """Validate and convert addresses dynamically via ArgumentParser"""
+    """Validate strings and convert to IPv4Network objects as they're parsed"""
 
     def __call__(self, parser, namespace, values, option_string=None):
-        # ascii_net_list = values.split(',')
         for net in values:
             self._validate_network(parser, net)
         setattr(
@@ -179,17 +122,18 @@ class CSVNetworkList(Action):
             list(map(ip_network, values))
         )
 
-    def _validate_network(self, parser, net):
+    def _validate_network(self, parser, net, ip_version=None):
+        if ip_version is None:
+            ip_version = DEFAULT_IP_VERSION
         try:
             network = ip_network(net)
-            if network.version != SUPPORTED_IP_VERSION:
+            if network.version != ip_version:
+                print(network.version)
                 parser.error('Only IPv4 is supported. To add IPv6 support '
-                             'yourself, start by changing ALL_ADDRESS')
+                             'yourself, start by changing references to IPV4_ALL_NET')
             return network
         except ValueError:
-            print()
-            print('BAD ADDRESS: Invalid network in list: {}'.format(net))
-            print()
+            error('BAD ADDRESS: Invalid network in list: {}'.format(net))
             if '/' in net:
                 parser.error('Ensure that no host bits are set in CIDR notation'
                              'network addresses, or use "--loose"')
@@ -232,25 +176,32 @@ def cli(appname):
         help='Output a QR code to the screen using ANSI escape chars')
     argparser.add_argument(
         '-l', '--lines-output',
-        dest='lines',
+        dest='enable_lines',
         action='store_true',
         default=False,
         required=False,
         help='Produce output in line-based format')
     argparser.add_argument(
-        '-4', '--include-ip4-only',
-        dest='ip4_only',
+        '-4', '--no-ip6',
+        dest='no_ip6',
         action='store_true',
         default=False,
         required=False,
-        help='Do not include ::0/0 in AllowedIPs output')
+        help='Do not include {} output'.format(IPV6_ALL_NET))
+    argparser.add_argument(
+        '-W', '--no-wireguard-format',
+        dest='wireguard_format',
+        action='store_false',
+        default=True,
+        required=False,
+        help='Exclude the "AllowedIPs = " literal')
     argparser.add_argument(
         '-C', '--no-csv-output',
         dest='disable_csv',
         action='store_true',
         default=False,
         required=False,
-        help='Disable output of single-line CSV')
+        help='Disable output of single-line CSV (WireGuard-style) format')
 
     args = argparser.parse_args()
 
@@ -266,23 +217,19 @@ def cli(appname):
 
 def main():
     """Get command-line arguments and output results in specified formats"""
-    # Too lazy to use Python logging
-    global G_DEBUG
     args = cli(argv[0])
-    G_DEBUG = args.debug
-
     excluded_network_list = args.excluded_network_list
-    inclusive_netlist = exclude_net_list(excluded_network_list, strict=args.strict)
+    inclusive_netlist = exclude_nets_to_include_nets(excluded_network_list, strict=args.strict)
+
+    if args.no_ip6 is False:
+        inclusive_netlist.append(ip_network_safe(IPV6_ALL_NET))
 
     csv_inclusive_netlist = ','.join(
         [included_net.compressed for included_net in inclusive_netlist])
 
-    if args.ip4_only is False:
-        csv_inclusive_netlist += ',::0/0'  # Force all IPv6 through the tunnel
-
     if args.qrencode is True:
         qr_encode_command = QR_ENCODE_COMMAND + [csv_inclusive_netlist]
-        debug('Generating WireGuard friendly QR code format:')
+        output('Generating WireGuard friendly QR code format:')
         try:
             result = run(qr_encode_command, stdout=PIPE, check=True)
         except CalledProcessError as err:
@@ -290,15 +237,16 @@ def main():
         output(result.stdout.decode('utf-8'))
         output('')
 
-    if args.lines:
-        debug('Line-based output:')
+    if args.enable_lines:
+        output('Line-based output:')
         for included_net in [net.compressed for net in inclusive_netlist]:
             output('{}'.format(included_net))
 
     if args.disable_csv is False:
-        debug('CSV line output:')
-        output('AllowedIPs = {}{}'.format(
-            csv_inclusive_netlist, ',::0/0' if args.ip4_only is False else ''))
+        output('-- CSV line output --')
+        output('{}{}'.format(
+            'AllowedIPs = ' if args.wireguard_format is True else '',
+            csv_inclusive_netlist))
 
 
 if __name__ == '__main__':
